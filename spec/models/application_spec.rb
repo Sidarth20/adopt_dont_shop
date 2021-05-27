89f59b412261764f5b7e25e2e40350b241bffb1a
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-26 19:09:40 -0600

    Update Partial Matches for Pet Names

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe Application, type: :model do
     it {should have_many(:pets).through(:application_pets)}
   end
 
+  describe 'class methods' do
+    describe '#search' do
+      it 'returns partial matches' do
+      # Partial Matches for Pet Names
+      # As a visitor
+      # When I visit an application show page
+      # And I search for Pets by name
+      # Then I see any pet whose name PARTIALLY matches my search
+      # For example, if I search for "fluff", my search would match pets with names "fluffy", "fluff", and "mr. fluff"
+        @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+
+        expect(Pet.search("Claw")).to eq([@pet_2])
+      end
+    end
+  end
+
   describe 'instance methods' do
     it 'should default application_status to in progress' do
       application = Application.new
