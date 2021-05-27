deee2b63398561105391dd245c916b92f8db8e7c
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-26 19:14:12 -0600

    Create test for case insensitive matches

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -22,6 +22,21 @@ RSpec.describe Application, type: :model do
 
         expect(Pet.search("Claw")).to eq([@pet_2])
       end
+
+      it 'returns case insensitivie matches' do
+        # Case Insensitive Matches for Pet Names
+        # As a visitor
+        # When I visit an application show page
+        # And I search for Pets by name
+        # Then my search is case insensitive
+        # For example, if I search for "fluff", my search would match pets with names "Fluffy", "FLUFF", and "Mr. FlUfF"
+        @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+
+        expect(Pet.search("cLaW")).to eq([@pet_2])
+      end
     end
   end
 
